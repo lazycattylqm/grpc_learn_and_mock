15bd44ed983b2418f53b3ff81f44f0fb1ede78d3
qmli <leeqiming@163.com>
2021-09-15 13:58:02 +0800

    add change for golang grpc

---
+++ go/client/client_main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	pb "example.com/liqiming/client/proto"
+	"google.golang.org/grpc"
+	"log"
+)
+
+const (
+	addr = "localhost:3000"
+	name = "liqiming"
+)
+func main() {
+	dial, err := grpc.Dial(addr, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("grpc dial failed %v", err)
+	}
+	defer dial.Close()
+	client := pb.NewHelloServiceClient(dial)
+	hello, serviceErr := client.SayHello(
+		context.Background(), &pb.HelloRequest{
+			Name: name,
+		},
+	)
+	if serviceErr != nil {
+		log.Fatalf("calling service error %v", serviceErr)
+	}
+	marshal, jsonErr := json.Marshal(hello)
+	if jsonErr != nil {
+		log.Fatalf("json error ,  %v", jsonErr)
+	}
+	log.Printf("service response is %v\n", string(marshal))
+
+}
---
+++ go/server/service/hello_service.go
@@ -0,0 +1,19 @@
+package service
+
+import (
+	"context"
+	pb "example.com/liqiming/proto"
+	"fmt"
+)
+
+type HelloService struct {
+	pb.UnimplementedHelloServiceServer
+}
+
+func (s *HelloService) SayHello(ctx context.Context, request *pb.HelloRequest) (*pb.HelloReply, error)  {
+	name := request.Name
+	helloMsg := fmt.Sprintf("Hello %s, this is server", name)
+	return &pb.HelloReply{
+		Message:  helloMsg,
+	}, nil
+}
