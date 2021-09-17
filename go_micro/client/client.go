8fe56e1fb5b04030b9a7b1deb2fa5a4bdd2f3b4c
qmli <leeqiming@163.com>
2021-09-17 23:25:41 +0800

    fix go micro part

--- go_micro/client/client.go
+++ go_micro/client/client.go
@@ -9,12 +9,15 @@ import (
 )
 
 func main() {
-	service := micro.NewService()
+	service := micro.NewService(
+		micro.Name("hello.server"),
+		micro.Address(":3000"),
+		)
 	service.Init()
 	helloService := helloPb.NewHelloService("hello.server", service.Client())
 	sayHello, err := helloService.SayHello(
 		context.Background(), &helloPb.HelloRequest{
-			Name: "liqiming client",
+			Name: "liqiming client go micro",
 		},
 	)
 	if err != nil {
